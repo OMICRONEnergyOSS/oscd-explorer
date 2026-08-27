7baa9d0e61424de2fdcb88575e36ca12692a6feb
Stephen Renwick <stephen.renwick@omicronenergy.com>
2026-08-27 17:04:54 +0200

    Updates

---
+++ assets/de-sPDLLFv4.js
@@ -0,0 +1,19 @@
+// Do not modify this file by hand!
+// Re-generate this file by running lit-localize
+import { str } from '@lit/localize';
+/* eslint-disable no-irregular-whitespace */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export const templates = {
+    's44851a8adf059eef': `Angeheftet`,
+    's5d929ff1619ac0c9': `Suche`,
+    's6d8c02aee480af7a': `Menü`,
+    's7293d04c5979a8ad': `Seitenleiste ausklappen`,
+    's7417792bbe720149': `Wiederholen`,
+    's8f10b4e57121e256': `Seitenleiste einklappen`,
+    's8f4be9f086eb530f': `Rückgängig`,
+    'sd1fdd3cc8714d686': str `${0} anheften`,
+    'sf8256ec799969cb7': str `${0} lösen`,
+    's3e2b2947e76caf9c': `Items you pin will appear here`,
+    's5f4586bc1e2740e6': `Clear search`,
+};
+//# sourceMappingURL=de.js.map
