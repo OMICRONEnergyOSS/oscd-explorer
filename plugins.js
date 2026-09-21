59f01498a67c370838bde8c95ced72af9a3b7d4f
Stephen Renwick <stephen.renwick@omicronenergy.com>
2026-09-21 16:38:28 +0200

    feat: switch to a tweaked fork of the plugins-hub

--- plugins.js
+++ plugins.js
@@ -1,6 +1,7 @@
 import "@webcomponents/scoped-custom-element-registry";
 import OscdMenuOpen from "@omicronenergy/oscd-menu-open";
 import OscdMenuNew from "@omicronenergy/oscd-menu-commons/oscd-menu-new.js";
+// import PluginsHub from "@compas-bearingpoint/plugins/dist/apps/plugins-hub/index.js";
 import OscdBackgroundPluginConfig from "@omicronenergy/oscd-background-plugin-config";
 import OscdBackgroundEditV1 from "@omicronenergy/oscd-background-editv1";
 import OscdBackgroundWizardEvents from "@omicronenergy/oscd-background-wizard-events/oscd-background-wizard-events.js";
@@ -10,6 +11,7 @@ const oscdShell = document.querySelector("oscd-shell");
 const { registry } = oscdShell;
 registry.define("oscd-menu-open", OscdMenuOpen);
 registry.define("oscd-menu-new", OscdMenuNew);
+// registry.define("plugins-hub", PluginsHub);
 registry.define("plugin-config", OscdBackgroundPluginConfig);
 registry.define("oscd-background-editv1", OscdBackgroundEditV1);
 registry.define("oscd-background-wizard-events", OscdBackgroundWizardEvents);
@@ -37,7 +39,9 @@ export const plugins = {
       translations: { de: "Plugin Hub" },
       icon: "edit",
       requireDoc: false,
-      src: "https://ase-compas.github.io/compas-bearingpoint-plugins/compas/plugins/plugins-hub/index.js",
+      // src: "https://ase-compas.github.io/compas-bearingpoint-plugins/compas/plugins/plugins-hub/index.js",
+      src: "https://stee-re.github.io/compas-bearingpoint-plugins/compas/plugins/plugins-hub/index.js",
+      // tagName: "plugins-hub",
     },
   ],
   background: [
